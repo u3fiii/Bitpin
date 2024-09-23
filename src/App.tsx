abc0711a1593abbf30e10f70e681ae8e5b2243a2
u3fiii <u3fi.ali@gmail.com>
2024-09-23 08:32:01 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Advertisements from "./Advertisements";
+function App() {
+  return (
+    <div className="App">
+      This is app
+      <Advertisements />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.bitpin.org", // The base URL of the API
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // Remove the /api prefix
+      },
+    },
+  },
+});
